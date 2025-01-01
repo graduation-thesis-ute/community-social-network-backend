9d2fce8b36b5af54b1724dd5aceb594f51295356
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2025-01-01 17:44:37 +0700

    fix: fix type of secret token

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -2,23 +2,23 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 const generateAccessToken = (payload: object) => {
-  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET || "", {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET as string, {
     expiresIn: process.env.ACCESS_TOKEN_LIFE,
   });
 };
 
 const generateRefreshToken = (payload: object) => {
-  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET || "", {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET as string, {
     expiresIn: process.env.REFRESH_TOKEN_LIFE,
   });
 };
 
 const verifyAccessToken = (token: string) => {
-  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET || "");
+  return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string);
 };
 
 const verifyRefreshToken = (token: string) => {
-  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET || "");
+  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET as string);
 };
 export {
   generateAccessToken,
